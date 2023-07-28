32ff2dbf463a81282a6d97a064cc238c2c256596
Kidus Nesibu <118051709+Kidus-Nesibu@users.noreply.github.com>
2023-07-28 16:12:35 +0300

    answer for hour 12 exersice 4

---
+++ hour 12/Exersice-4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char array_ch[10] = {'I',' ', 'l','i','k','e',' ','C','!'};
+    for(i=0;i<10;i++)
+        printf("%c", array_ch[i]);
+    return 0;
+}
