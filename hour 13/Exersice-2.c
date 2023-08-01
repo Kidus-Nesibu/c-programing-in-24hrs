9d8dcbcca86dc4568a32f0a1ca0a32d9b8ce2440
Kidus Nesibu <118051709+Kidus-Nesibu@users.noreply.github.com>
2023-08-01 11:51:06 +0300

    answer for hour 13 exercise 2

--- hour 13/Exersice-2.c
+++ hour 13/Exersice-2.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    char ch[]= "Hello there!";
-    printf("length of the array is %d\n", strlen(ch));
+    char ch[] = "Hello there!";
+    int i;
+    int length =0;
+    for (i=0;ch[i];i++)
+    {
+        length++;
+    }
+    printf("%d", length);
 }
